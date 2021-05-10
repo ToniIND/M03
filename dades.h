963ecc56215ab4b71e728b8ae468f0bf488f4cb5
Ins. Ausiàs March v180704 <tonichornetanducas@gmail.com>
2021-05-10 13:02:05 +0200

    Progres

--- dades.h
+++ dades.h
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
 /*-------------------------------------------------------------*/
 /*DEFINES                                                      */
@@ -19,6 +22,7 @@ struct usuaris {
 	int dinersJugats;
 	int idioma;
 	int any;
+	int colla;
 } usuari1;											   
 
 /*-------------------------------------------------------------*/
@@ -83,6 +87,13 @@ void infoJugador(){
 		strcpy (linies[i++], linia);
 	}
 	
+	printf("%s", linies[6]);
+	do {
+		scanf("%d", &usuari1.colla);
+		if (usuari1.colla < 0 && usuari1.colla > 1)
+			printf("%s", linies[7]);
+	} while (usuari1.colla < 0  && usuari1.colla > 1);
+	
 	printf("\n%s", linies[0]);
 	do {
 		scanf("%d", &usuari1.numeroBillet);
@@ -103,6 +114,67 @@ void infoJugador(){
 		if (usuari1.any <= 1812 || usuari1.any >= 2021) 
 			printf("%s", linies[5]);
 	} while (usuari1.any <= 1812 || usuari1.any >= 2021);
+	
+}
+
+/*-------------------------------------------------------------*/
+
+
+
+/*-------------------------------------------------------------*/
+/*FUNCIO: Demana l'informació de l'usuari                      */
+
+void generarPremis(){
+	
+	int i, j, coincide, temp = 0, randomIndex = 0, premis[99999], nPremis = 1807;
+    char nom[100];
+    FILE * fp;
+    
+    srand(time(NULL));
+    
+	//GENEREM L'ARRAY
+		
+	for (i = 0; i < nPremis; i++) {
+		premis[i] = rand()%99999;
+		coincide=0;
+		for (j = 0 ; j < i && !coincide; j++) {
+			if (premis[i] == premis[j]) {
+				coincide=1;
+				i--;
+			}
+		}
+	}
+	 
+	// ALEATORITZEM L'ARRAY
+		
+	for (int i = 0; i < nPremis ; i++) {
+		temp = premis[i];
+		randomIndex = rand() % nPremis;
+	 
+		premis[i] = premis[randomIndex];
+		premis[randomIndex] = temp;
+			
+	}
+	
+	snprintf(nom, 100, "%d.dat", usuari1.any);
+	
+	// Obrim fitxer escriptura
+	
+	fp = fopen(nom,"w"); 
+	fwrite(premis, sizeof(int), nPremis, fp);
+	fclose(fp);
+		
+		
+	// Obrim fitxer de lectura
+	
+	fp = fopen(nom, "r"); 
+	for (i=0; i<nPremis; i++) {  
+		fread(premis, sizeof(int), nPremis, fp);      
+		printf ("%05d\n", premis[i]);
+	}
+		
+	fclose(fp);
+	
 }
 
 /*-------------------------------------------------------------*/
