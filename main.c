12b42b3d7454992289c89ccb20f26146679fbdd2
ToniIND <32127242+ToniIND@users.noreply.github.com>
2021-05-18 11:55:37 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,70 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-#include <string.h>
 #include "dades.h"
-
-
-/*-------------------------------------------------------------*/
-/*FUNCIO: Demana l'informació de l'usuari                      */
-
-void generarPremis(){
-	
-	int i, j, coincide, temp = 0, randomIndex = 0, premis[99999], nPremis = 1807;
-    char nom[100];
-    FILE * fp;
-    
-    srand(time(NULL));
-    
-	//GENEREM L'ARRAY
-		
-	for (i = 0; i < nPremis; i++) {
-		premis[i] = rand()%99999;
-		coincide=0;
-		for (j = 0 ; j < i && !coincide; j++) {
-			if (premis[i] == premis[j]) {
-				coincide=1;
-				i--;
-			}
-		}
-	}
-	 
-	// ALEATORITZEM L'ARRAY
-		
-	for (int i = 0; i < nPremis ; i++) {
-		temp = premis[i];
-		randomIndex = rand() % nPremis;
-	 
-		premis[i] = premis[randomIndex];
-		premis[randomIndex] = temp;
-			
-	}
-	
-	snprintf(nom, 100, "%d.dat", usuari1.any);
-	
-	// Obrim fitxer escriptura
-	
-	fp = fopen(nom,"w"); 
-	fwrite(premis, sizeof(int), nPremis, fp);
-	fclose(fp);
-		
-		
-	// Obrim fitxer de lectura
-	/*
-	fp = fopen(nom, "r"); 
-	for (i=0; i<nPremis; i++) {  
-		fread(premis, sizeof(int), nPremis, fp);      
-		printf ("%05d\n", premis[i]);
-	}
-		
-	fclose(fp);
-	*/
-}
-
-/*-------------------------------------------------------------*/
-
-
 
 /*-------------------------------------------------------------*/
 /*FUNCIO: Main del programa                                    */
@@ -75,7 +10,7 @@ int main (void) {
 	
 	idiomes();
 	
-	printf("Vols jugar sol o amb colla? (0 - SOL / 1 - COLLA): ");
+	printf("\n%s", linies[7]);
 	do {
 		scanf("%d", &format);
 		if (format == 0) {
@@ -90,5 +25,3 @@ int main (void) {
 }
 
 /*-------------------------------------------------------------*/
-
-
